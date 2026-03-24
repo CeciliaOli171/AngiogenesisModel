12d6affd203b945769702a3b7893ea6207f230c7
CeciliaOli171 <coli171@aucklanduni.ac.nz>
2026-03-24 15:17:28 +1300

    Pybind implementation

--- src/CouplingFetoflowChaste.hpp
+++ src/CouplingFetoflowChaste.hpp
@@ -1,33 +1,33 @@
-#ifndef COUPLINGFETOFLOWCHASTE_HPP_
-#define COUPLINGFETOFLOWCHASTE_HPP_
+// #ifndef COUPLINGFETOFLOWCHASTE_HPP_
+// #define COUPLINGFETOFLOWCHASTE_HPP_
 
-#include <pybind11/pybind.h>
-#include <pybind11/stl.h>
-#include <vector>
+// #include <pybind11/pybind11.h>
+// #include <pybind11/stl.h>
+// #include <vector>
 
-namespace py = pybind11;
+// namespace py = pybind11;
 
-/**
- * TO MODIFY!!!! 
- * A class for converting c_vector into python list and vice verso to use the python library Fetoflow for flow calculations.
- */
+// /**
+//  * TO MODIFY!!!! 
+//  * A class for converting c_vector into python list and vice verso to use the python library Fetoflow for flow calculations.
+//  */
 
-template class<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
-class CouplingFetoflowChaste : 
-{
-private:
-    // values ?
+// template class<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+// class CouplingFetoflowChaste : 
+// {
+// private:
+//     // values ?
 
-public:
+// public:
 
-    CouplingFetoflowChaste();
+//     CouplingFetoflowChaste();
 
-    ~CouplingFetoflowChaste();
+//     ~CouplingFetoflowChaste();
 
-    py::list ChasteDataForFetoflow(std::vector<double> v);
+//     py::list ChasteDataForFetoflow(std::vector<double> v);
 
-    std::vector<double> FetoflowDataForChaste(const py::iterable& l);
+//     std::vector<double> FetoflowDataForChaste(const py::iterable& l);
     
-};
+// };
 
-#endif /* COUPLINGFETOFLOWCHASTE_ */
+// #endif /* COUPLINGFETOFLOWCHASTE_ */
